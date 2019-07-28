e2062e6bba01f0c86469490e46ec7b0ed47884f9
LouZhangpeng <louzp@sina.cn>
2019-07-28 12:55:47 +0800

    regular commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define DIVIDOR 10
+#define M 1000
+
+int main()
+{
+    printf("Hello, world!\n");
+    
+    int a[M] = {0};
+    for (int i = 0; i < M; i++) {
+        a[i] = i % DIVIDOR;
+        printf("%.4d", a[i]);
+        if (i % 20 == 0) printf("\n");
+    }
+
+    return 0;
+}
